8b4379fdbc8731c3071102bf802c5afa8d6c567b
Nabeel <cpmohammednabeel@gmail.com>
2021-10-15 11:57:45 +0530

    Added sale customization module

---
+++ aks_sale_customization/__manifest__.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+#    Author: ALKIDHMA
+#    you can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    It is forbidden to publish, distribute, sublicense, or sell copies
+#    of the Software or modified copies of the Software.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    GENERAL PUBLIC LICENSE (LGPL v3) along with this program.
+#    If not, see <https://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': "Sale Customization",
+    'version': '15.0.0.0',
+    'category': 'Sale',
+    'summary': 'Appointment Customization',
+    'live_test_url': 'Add youtube video Link',
+    'author': 'Al Khidma Systems',
+    'license': 'OPL-1',
+    'price': '',
+    'currency': 'USD',
+    'maintainer': 'Al Khidma Systems',
+    'support': 'tech@alkhidmasystems.com',
+    'website': "http://alkhidmasystems.com",
+    'depends': [
+        'sale'
+    ],
+    'data': [
+        'data/sequence_data.xml',
+        'views/sales_order.xml',
+        'report/sale_report_templates.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
