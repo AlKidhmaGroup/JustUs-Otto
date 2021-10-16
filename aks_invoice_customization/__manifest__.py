64de2ef57c62adcf0b2d0f2f56fdc2128ae5a160
Shahinshah Ummer <shahinshaummer83@gmail.com>
2021-10-16 18:51:42 +0530

    module aks invoice customization.

---
+++ aks_invoice_customization/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Invoice Customization",
+
+    'summary': """
+    """,
+
+    'description': """
+    """,
+
+    'author': "Alkhidma",
+    'website': "http://www.alkhidmasystems.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'invoice',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['account'],
+
+    # always loaded
+    'data': [
+        'views/account_move.xml',
+        'views/company_view.xml',
+        'reports/invoice_print_report.xml',
+        'reports/invoice_print.xml',
+    ],
+}
