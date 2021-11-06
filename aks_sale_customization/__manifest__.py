22a97517205ccd0941ccac1f592b525b8b495ed8
Shabeeb c s <csshabeeb@gmail.com>
2021-11-06 13:49:30 +0530

    [Fix] the analytic account creation in project

--- aks_sale_customization/__manifest__.py
+++ aks_sale_customization/__manifest__.py
@@ -22,7 +22,7 @@
 
 {
     'name': "Sale Customization",
-    'version': '15.0.0.2',
+    'version': '15.0.0.3',
     'category': 'Sale',
     'summary': 'Sale Customization',
     'live_test_url': 'Add youtube video Link',
